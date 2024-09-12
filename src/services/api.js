4f89a4af67025e754470342f14f925744116b140
JoseRo295 <joselrc295@gmail.com>
2024-09-11 21:13:00 -0500

    nueva ruta

--- src/services/api.js
+++ src/services/api.js
@@ -5,24 +5,27 @@ export default class Api {
   token = "";
 
   constructor() {
-    this.urlBase = "http://localhost:3001/";  
+    this.urlBase = process.env.REACT_APP_API_URL || "http://localhost:3000";
   
     this.token = localStorage.getItem("user_logged");
     if (this.token) {
       axios.defaults.headers.common["Authorization"] = "Bearer " + this.token;
     }
   }
+
   post = (url, data) => {
     return axios.post(this.urlBase + url, data);
   };
+
   get = (url) => {
     return axios.get(this.urlBase + url);
-    }; 
-  patch =(url, data)=>{
-    return axios.patch(this.urlBase + url, data);
-  }
-  delete = (url) => {
-    return axios.delete(this.urlBase + url );
-    };
-}
+  };
 
+  patch = (url, data) => {
+    return axios.patch(this.urlBase + url, data);
+  };
+
+  delete = (url) => {
+    return axios.delete(this.urlBase + url);
+  };
+}
